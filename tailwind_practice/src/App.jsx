3dfdeada8d9d65b9de747cfd1661c89c8decce7c
Sameer <sameer.marathe16536@sakec.ac.in>
2024-06-18 21:04:23 +0530

    responsiveness

--- tailwind_practice/src/App.jsx
+++ tailwind_practice/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Grids from './Grids'
+import Responsive from './Responsive'
 function App() {
   
 
@@ -16,6 +17,7 @@ function App() {
       </div>
       <br />
       <Grids />
+      <Responsive />
     </>
   )
 }
---
+++ tailwind_practice/src/Responsive.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Responsive = () => {
+  return (
+    <div className='bg-red-500 md:bg-blue-500 lg:bg-yellow-500 text-center'>Responsive</div>
+  )
+}
+
+export default Responsive
